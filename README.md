4cbdcf657bec2e224df08110ff0b18ab58504be5
gauravl-tevaeralabs <gauravl@tevaeralabs.com>
2024-05-29 12:36:42 +0530

    Add code to demonstrate zksync paymaster using zksync-web3 & zksync-ethers sdk

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # zksync-paymaster-demo
+
+## About
+
+The project demonstrates the implementation of a zkSync paymaster using zkSync-web3 (currently deprecated) and zkSync-ethers. However, it demonstrates an issue with zkSync-ethers, whereas the same code and paymaster work fine with the zkSync-web3 SDK.
+
+## Getting Started
+
+1. Make sure you have NodeJS >=v20 installed.
+2. Clone this repo.
+3. In your terminal window execute the following command from the current folder location:
+
+```
+npm install
+```
+
+4. Create a config.json file (if not exist) in the zksync-ethers & zksync-web3 folders, with the following content:
+
+```
+{
+  "zksyncRpcUrl": "<zksync_rpc_url>",
+  "paymasterContractAddress": "<paymaster_contract_address>",
+  "tevaQuestRegistryContractAddress": "<teva_quest_registry_contract_address>"
+}
+```
+
+5. Now execute the following in your terminal window:
+
+```
+npm run start
+```
+
+This will run the function.
---
+++ zksync-web3/index.js
@@ -0,0 +1,70 @@
+/* jshint esversion: 11 */
+
+// Import the dependency.
+const { Provider, Wallet, utils, Contract } = require("zksync-web3");
+
+const config = require("./config.json");
+
+const zksyncRpcUrl = config.zksyncRpcUrl;
+const paymasterContractAddress = config.paymasterContractAddress;
+const tevaQuestRegistryContractAddress =
+  config.tevaQuestRegistryContractAddress;
+
+async function registerQuest(connectedWallet) {
+  // create quest registry contract instance
+  const tevaQuestRegistry = new Contract(
+    tevaQuestRegistryContractAddress,
+    require("./teva-quest-registry.abi.json"),
+    connectedWallet
+  );
+
+  // create paymaster params for general flow
+  const paymasterParams = utils.getPaymasterParams(paymasterContractAddress, {
+    type: "General",
+    innerInput: new Uint8Array(),
+  });
+
+  try {
+    // call register with paymaster params
+    const txn = await tevaQuestRegistry.register({
+      customData: {
+        paymasterParams,
+        gasPerPubdata: utils.DEFAULT_GAS_PER_PUBDATA_LIMIT,
+      },
+    });
+    const { hash } = txn;
+
+    console.log(
+      "Successfully registered quest using paymaster! Txn hash: %s",
+      hash
+    );
+  } catch (error) {
+    console.warn("Error occurred while quest registering: %s", error.message);
+  }
+}
+
+async function registerQuestUsingPaymaster() {
+  // create zksync provider
+  const provider = new Provider(zksyncRpcUrl);
+  // generate random wallet
+  const wallet = Wallet.createRandom();
+  // connect wallet to provider
+  const connectedWallet = wallet.connect(provider);
+
+  // register quest with newly generated wallet
+  await registerQuest(connectedWallet);
+}
+
+// Handler
+async function run() {
+  try {
+    await registerQuestUsingPaymaster();
+  } catch (error) {
+    console.error(
+      "Error occurred while executing transaction. error: %s",
+      error.message
+    );
+  }
+}
+
+run();
